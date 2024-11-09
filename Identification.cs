834688b0a5716f313bdcee89a8f21d631157f379
SKhorramzadeh <s.khorramzadeh@gmail.com>
2024-11-09 11:15:09 +0330

    Add files via upload

---
+++ Identification.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Test1       //Identification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please Enter your name");
+            name=Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            
+            string strAge;
+            int age;
+            Console.WriteLine("Please Enter your Age");
+            strAge = Console.ReadLine();
+            age = int.Parse(strAge);
+            if (age<15 || age>120)       // !=not ||=or
+            {
+                Console.WriteLine("Your Age must be between 15 and 120");
+            }
+            else
+            {
+                Console.WriteLine($"Your Age is {strAge}");
+            }
+            Console.WriteLine("Please Enter your Age");
+            for (; ; )                 // for(int i=0 ; i<= j ; i++)
+            {
+                strAge = Console.ReadLine();
+                age = int.Parse(strAge);
+                if (age >= 15 && age <= 120)
+                {
+                    Console.WriteLine($"Your Age is {strAge}");
+                    break;
+                }
+                Console.WriteLine("Your Age must be between 15 and 120; Enter again");
+            }
+
+            string strPhoneNumber;
+            long intPhoneNumber;
+            Console.WriteLine("Please Enter your Phone Number");
+            strPhoneNumber = Console.ReadLine();
+
+            while (true)
+            {
+                if (strPhoneNumber.Length==11)
+                    break;
+                Console.WriteLine("Phone Number must have 11 digit; try again");
+                strPhoneNumber = Console.ReadLine();
+            }
+
+            do
+            {
+                if (strPhoneNumber.StartsWith("0"))
+                    break;
+                Console.WriteLine("Phone Number must start by 0; try again");
+                strPhoneNumber = Console.ReadLine();
+
+            } while (true);
+
+            bool digit = true;
+            while (digit)
+            {
+                try
+                {
+                    intPhoneNumber = long.Parse(strPhoneNumber);
+                    digit = false;
+                }
+                catch   //(Exception)
+                {
+                    Console.WriteLine("Phone Number must just countain digits; try again");
+                    strPhoneNumber = Console.ReadLine();
+                    //throw;
+                }
+            }
+        }
+    }
+}
---
+++ Mathematics.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Test2           //Mathematics (Divisiblity, Sorting)
+{
+    class Program
+    {
+        //A method to find the lowest number in an array (with 2 private outputs).
+        public static (int, int) minVal(int n, int[] ary)
+        {
+            int minValue = ary[0];
+            int minID = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (ary[j] < minValue)
+                    {
+                        minValue = ary[j];
+                        minID = j;
+                    }
+                }
+            }
+            return (minValue, minID);
+        }
+
+        //A method to find the bigest number in an array .
+        public static int maxVal(int n, int[] ary)
+        {
+            int maxValue = ary[0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (ary[j] > maxValue)
+                    {
+                        maxValue = ary[j];
+                    }
+                }
+            }
+            return maxValue;
+        }
+
+        //check if 2 digits are divisible or not?
+        static void Main(string[] args)
+        {
+            string strNum;
+            int num1;
+            int num2;
+            double rest;
+            Console.WriteLine("Please Enter 2 digits to check if they are divisible or not?");
+            strNum = Console.ReadLine();
+            num1 = int.Parse(strNum);
+            strNum = Console.ReadLine();
+            num2 = int.Parse(strNum);
+            if (num1 > num2)
+                rest = num1 % num2;
+            else
+                rest = num2 % num1;
+            if (rest == 0)
+                Console.WriteLine($"{num1} & {num2} are divisible");
+            else
+                Console.WriteLine($"The rest of dividion of {num1} & {num2} is {rest}");
+
+            //Sorting an array.
+            string str;
+            int nDigits;
+            Console.WriteLine("Please Enter an aray to sort its argumants");
+            Console.WriteLine("First Enter number of your digits");
+            str = Console.ReadLine();
+            nDigits = int.Parse(str);
+            int[] array1 = new int[nDigits];
+            int[] array2 = new int[nDigits];
+
+            for (int i = 0; i < nDigits; i++)       // Read array
+            {
+                str = Console.ReadLine();
+                array1[i] = int.Parse(str);
+                array2[i] = array1[i];
+            }
+            int maximum = maxVal(nDigits, array1);  //Find Max
+            for (int i = 0; i < nDigits; i++)      //Find min
+            {
+                (int minimum, int mID) = minVal(nDigits, array1);
+                array2[i] = minimum;
+                array1[mID] = maximum;
+            }
+            for (int i = 0; i < nDigits; i++)     //Print
+            {
+                Console.WriteLine($" {i} , {array2[i]}");
+            }
+        }
+    }
+}
